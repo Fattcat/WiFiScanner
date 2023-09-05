c00656a6efb30cadfea60442bd80834765b9e57f
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-09-05 19:39:56 +0200

    Update Scan.py

--- Scan.py
+++ Scan.py
@@ -2,6 +2,15 @@ import time
 import pywifi
 from pywifi import const
 
+def freq_to_channel(freq):
+    # Convert frequency to channel number
+    if 2412 <= freq <= 2484:
+        return (freq - 2412) // 5 + 1
+    elif 5180 <= freq <= 5825:
+        return (freq - 5180) // 5 + 36
+    else:
+        return None
+
 def scan_wifi():
     wifi = pywifi.PyWiFi()
     iface = wifi.interfaces()[0]
@@ -13,16 +22,18 @@ def scan_wifi():
     for result in scan_results:
         ssid = result.ssid
         bssid = result.bssid
-        channel = result.channel
+        freq = result.freq
+        channel = freq_to_channel(freq)
         security_type = result.akm[0]
         signal_strength = result.signal
-
+        print("+" + "--" * (len(bssid) - 3) + "+")
         print("WiFi (SSID):", ssid)
         print("Mac Address:", bssid)
         print("Channel:", channel)
         print("Security Type:", security_type)
         print("WiFi Signal Strength:", signal_strength)
-        print()
+        print("+" + "--" * (len(bssid) - 3)  + "+")
+        print("\n")
 
 def main():
     while True:
